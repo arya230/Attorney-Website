43c690149947b049c0808c2773ecbdef26e0be73
Arya Gairola <arya@Aryas-MacBook-Air.local>
2025-07-08 01:57:30 +0530

    subUrls section completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,7 @@ import img3 from "../assets/realestate4.avif"
 import img4 from "../assets/realestate5.png"
 import img5 from "../assets/realestate6.jpg"
 import img6 from "../assets/realestate7.jpeg"
+import suburlsImg from "../assets/sub-urls-img.avif"
 
 
 
@@ -75,8 +76,83 @@ const Home: React.FC = () => (
     <h2>Lakshmikumaran & Sridharan is a full-service law firm based in India. The firm has offices in 14 cities and has over 500 professionals specializing in areas such as corporate & commercial laws, dispute resolution, taxation and intellectual property.</h2>
     <div className="inner-content">
         <p>Over the last three decades, we have worked with a variety of clients – start-ups, small & medium enterprises, large Indian corporates and multinational companies.</p>
-        <p>Our professionals have experience of working in both traditional sectors such as commodities, automobile, pharmaceuticals, petrochemicals and modern sectors such as e-commerce, big data, renewables.</p>
+        <p className='second-div'>Our professionals have experience of working in both traditional sectors such as commodities, automobile, pharmaceuticals, petrochemicals and modern sectors such as e-commerce, big data, renewables.</p>
     </div>
+  </div>
+
+
+  <div className="sub-urls-section">
+    <img src={suburlsImg} alt="" />
+    <section>
+        <h2>Our Practices</h2>
+        <div className="col-wrapper">
+
+       
+        <div className="col1">
+        <ul>
+            <li>
+                <a href="#">Arbitration</a>
+            </li>
+            <li>
+                <a href="#">BIS</a>
+            </li>
+            <li>
+                <a href="#">Banking & Finance</a>
+            </li>
+            <li>
+                <a href="#">Commercial Litigation</a>
+            </li>
+            <li>
+                <a href="#">Competition and Antitrust</a>
+            </li>
+            <li>
+                <a href="#">Coporate</a>
+            </li>
+            <li>
+                <a href="#">Customs</a>
+            </li>
+            <li>
+                <a href="#">Direct tax</a>
+            </li>
+            <li>
+                <a href="#">Economic offences</a>
+            </li>
+        </ul>
+        </div>
+        <div className="col2">
+        <ul>
+            <li>
+                <a href="#">Employment Law</a>
+            </li>
+            <li>
+                <a href="#">Goods and Services Tax</a>
+            </li>
+            <li>
+                <a href="#">Insolvency</a>
+            </li>
+            <li>
+                <a href="#">Intellectual Property</a>
+            </li>
+            <li>
+                <a href="#">International Trade and WTO</a>
+            </li>
+            <li>
+                <a href="#">Mergers & Acquisitions/PE</a>
+            </li>
+            <li>
+                <a href="#">Real Estate Law</a>
+            </li>
+            <li>
+                <a href="#">Regulatory</a>
+            </li>
+            <li>
+                <a href="#">Securities Law</a>
+            </li>
+        </ul>
+        </div>
+        </div>
+    </section>
+
   </div>
 
   </>
