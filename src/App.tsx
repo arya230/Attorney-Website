1635aa8d374a4d6aaa826ee70e731cfcd868116d
Arya Gairola <arya@Aryas-MacBook-Air.local>
2025-07-16 15:16:50 +0530

    added gst page main content

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import People from './pages/people';
+import Gst from './pages/Practices/gst';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/people" element={<People/>} />
+        <Route path="/gst" element={<Gst/>} />
       </Routes>
     </Router>
   );
